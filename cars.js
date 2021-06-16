1444232c879f100329eefe432a3ecf75d0c64e21
Henrique <henrique.thsantos.ths@gmail.com>
2021-06-16 18:50:33 -0300

    Testes de Validaçao

---
+++ cars.js
@@ -0,0 +1,18 @@
+const obj = [
+    {
+        model: "Fiesta",
+        madeBy: "Ford",
+        year: 2015,
+    },
+    {
+        model: "Fusca",
+        madeBy: "VolksWagen",
+        year: NumberInt("2012"),
+    },
+    {
+        model: "Onix",
+        madeBy: "Chevrolet",
+        year: 2020,
+    },
+    { model: "Nave", madeBy: "Tesla", year: NumberInt(2060) },
+];
